8f427850d20d70f400473276ab618a8c0212c891
lubin <newlubin@163.com>
2019-08-01 14:04:54 +0800

    test(): 测试格式化

--- src/array/in_array.ts
+++ src/array/in_array.ts
@@ -13,7 +13,8 @@ import * as arrayValuesFunc from './array_values';
  * return boolean
  */
 export function inArray(
-  needle: string | number | boolean,  haystack: any,
+  needle: string | number | boolean,
+  haystack: any,
   strictMode: boolean = false
 ): boolean {
   let newHayStack = [];

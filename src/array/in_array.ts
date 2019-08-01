e0fd6f32ca88a0ec38e5ca00f1acf238c4206057
lubin <newlubin@163.com>
2019-08-01 14:06:25 +0800

    fix(): 测试格式化

--- src/array/in_array.ts
+++ src/array/in_array.ts
@@ -13,9 +13,7 @@ import * as arrayValuesFunc from './array_values';
  * return boolean
  */
 export function inArray(
-  needle: string | number | boolean,
-  haystack: any,
-  strictMode: boolean = false
+  needle: string | number | boolean,  haystack: any,  strictMode: boolean = false
 ): boolean {
   let newHayStack = [];
   if (haystack.constructor === Object) {
